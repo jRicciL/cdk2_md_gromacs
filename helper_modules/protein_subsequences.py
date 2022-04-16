1c92d941764cd7481fbe583a063ed7b29b89a6c7
Joel <ricci.lob@gmail.com>
2022-04-16 13:34:08 -0700

    Secondary structure sequence from UniProt included

--- helper_modules/protein_subsequences.py
+++ helper_modules/protein_subsequences.py
@@ -21,6 +21,20 @@ def get_pocket_residues(x='str', sep = ' '):
     final_seq = _get_seq(pocket_rangeResidues, x, sep)
     return(final_seq)
 
+def get_secondary_structure_residues(x='str', sep = ' '):
+    '''From https://www.uniprot.org/uniprot/P24941'''
+    sec_rangeResidues = [[3,12], [14,23], [29,34], [39,41], 
+                         [42,44], [46,57], [66,72], [75,81],
+                         [84,86], [87,93], [101, 120], 
+                         [130, 132], [133,135], [137,139],
+                         [141,143], [148,152], [159,161],
+                         [166,168], [171, 174], [178,180],
+                         [183,198], [208, 219], [230, 232],
+                         [248, 251], [252,254], [257,266],
+                         [271,273], [277,281], [284,286]]
+    final_seq = _get_seq(sec_rangeResidues, x, sep)
+    return(final_seq)
+
 def get_pisani_residues(x='str', sep = ' '):
     pisiani_rangeResidues = [ [4,12], [17, 24], [29,34], [46,55], [66,71], [76,81],  
                             [87,93], [101, 120], [121, 135], [140, 150], [182, 194], [277, 282]]
